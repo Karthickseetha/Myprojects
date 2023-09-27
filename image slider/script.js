47af8fb7cdcd7fc9cf6d8f788fa536e3651e81a9
Karthickseetha <91649083+Karthickseetha@users.noreply.github.com>
2023-09-27 21:34:33 +0530

    Add files via upload
    Experience the beauty of your favorite images with our image slider website. Whether you're showcasing your portfolio, sharing memories, or adding a touch of elegance to your site, our slider provides a stunning and customizable solution. Seamlessly switch between dark and light modes to suit your style and create an engaging visual experience for your visitors.
    Explore the power of images in a whole new light with our Image Slider website.
    Thank you.

---
+++ image slider/script.js
@@ -0,0 +1,25 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
+
+    let counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 3000);
+    
